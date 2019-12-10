0a3858f2209894c6eba39e2893c4955ad7b1baf9
ggx <1569413294@qq.com>
2019-12-10 10:53:29 +0800

    123

---
+++ src/main/java/com/bgs/demo11/ads.java
@@ -0,0 +1,17 @@
+package com.bgs.demo11;
+
+/**
+ * @ProjectName: demo11
+ * @Package: com.bgs.demo11
+ * @ClassName: ads
+ * @Author: Admin
+ * @Description:
+ * @Date: 2019/12/10 0010 10:52
+ * @Version: 1.0
+ */
+public class ads {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
